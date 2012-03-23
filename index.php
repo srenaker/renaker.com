c65482121221364f7dc5717b6a5c95efeb4d6f9d
S R <srenaker@mbpro.gateway.2wire.net>
2012-03-23 10:32:52 -0700

    first checkin

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+  <head>
+	<title>Stephen Renaker Harpsichords</title>
+	<link type="text/css" rel="stylesheet" href="/css/stylesheet.css" />
+	<style>
+	.intro_text {
+		padding-top: 0px;
+		padding-bottom: 24px;		
+		height: 357px;
+	}
+	</style>
+  </head>
+
+  <body>
+	<div class='header'><img src='/images/titlebar.png'></div>
+	<div class='backdrop'>
+
+	  <div class='intro_text'>
+		<p>
+	  	<span class='capital'>O</span>ver the last few years I've split my time between
+		harpsichord building and working at software startups, focusing more on one for awhile, then
+		going back to the other.  They're both interesting, and it's fun to move back and forth from 
+		the new economy to the (really) old economy. 
+		</p><p>
+		I spent three years as an apprentice in the studio of 
+		<a href='http://www.kevinfryer.net'>Kevin Fryer</a>, then worked as a software developer 
+		for two years at <a href='http://www.cohuman.com'>Cohuman</a>. Now I'm back in the workshop, building
+		  a new harpsichord (current progress <a href='gallery/op5'>here</a>).
+		</p><p>
+		I've built four instruments so far, the first two from kits and the most recent two from scratch.
+		I sold the first three, including the one pictured to the right, and the fourth is in 
+		my living room, where I try to keep the kids from destroying it.
+		</p>
+		<p><a href='gallery'>Image gallery</a>
+		&nbsp; 	&nbsp; 	&nbsp; 	&nbsp; 	
+		<a href='gallery/op3'>Audio/video</a></p>
+		<p><a href='slideshow/1'>Construction slideshow</a></p>
+	
+		
+		
+	  </div>
+	  <div class='image_lg'><a href='/gallery'><img src='/images/green.png' border='0'></a></div>
+
+	</div>
+<div class='footer'>
+	<p>
+  	Contact: <a href='mailto:info@renaker.com'>info@renaker.com</a>
+	</p>
+  </div> 
+	
+  </body>
+</html>
